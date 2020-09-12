cae0d7b49c6a89c564687f7689cf76690847327a
Kate Land <kateqland@gmail.com>
2020-09-12 12:08:30 -0400

    added a modal for submit report

--- src/components/Report.js
+++ src/components/Report.js
@@ -1,57 +1,89 @@
 import React from 'react';
 import '../styles/layout.css';
 
-function Report() {
-  return (
-    <div className="body">
-      <div className="column">
-        <form className="report">
-            <h3 style={{color: '#CD7320', fontSize: '28px', fontWeight: '200', textDecoration: 'underline'}}>Sexual Assault Report</h3>
-            <label for="university">What college/university do you attend?</label>
-            <input type="text" id="university"></input>
-            <label for="email">Please provide your school email address (must be .edu)</label>
-            <input type="text" id="email" ></input>
-            <label for="location">Location, date, and time of the event</label>
-            <input type="text" id="location"></input>
-            <label for="type">Why do you believe you were harassaed/discriminated against?</label>
-                <input className="checkbox" type="checkbox" id="type1"></input>
-                <label for="type1">Age</label>
-                <input className="checkbox" type="checkbox" id="type2"></input>
-                <label for="type2">National Origin</label>
-                <input className="checkbox" type="checkbox" id="type3"></input>
-                <label for="type3">Sexual Orientation</label>
-                <input className="checkbox" type="checkbox" id="type4"></input>
-                <label for="type4">Disability</label>
-                <input className="checkbox" type="checkbox" id="type5"></input>
-                <label for="type5">Pregnancy</label>
-                <input className="checkbox" type="checkbox" id="type6"></input>
-                <label for="type6">Gender</label>
-                <input className="checkbox" type="checkbox" id="type7"></input>
-                <label for="type7">Race</label>
-                <input className="checkbox" type="checkbox" id="type8"></input>
-                <label for="type8">Medical Condition</label>
-                <input className="checkbox" type="checkbox" id="type9"></input>
-                <label for="type9">Religion</label>
-                <input className="checkbox" type="checkbox" id="type10"></input>
-                <label for="type10">Other</label>
-                <label for="other">If other, explain.</label>
-                <input type="textarea" id="other"></input>
-            <label for="description">Describe the nature of the harassment</label>
-            <input type="textarea" id="description"></input>
-            <label for="offender">
-                If known, please state the person responsible for the harassment. 
-                If not, provide a description of the offender.
-            </label>
-            <input type="textarea" id="offender"></input>
-            <div className="row" style={{justifyContent: 'space-between', padding: '0px', width: '100%'}}>
-                <a href="#" ><button className="submit-button">Exit</button></a>
-                <button action="submit" className="submit-button">Submit</button>
-            </div>
-            
-        </form>
-      </div>
-    </div>
-  );
-}
 
-export default Report;
+    
+
+export default class Report extends React.Component {
+    createModal() {
+        // Set the modal text
+        var modal = document.getElementById("myModal");
+        var span = document.getElementsByClassName("close-modal")[0];
+       
+        // When the user clicks on the button, open the modal
+        modal.style.display = "flex";
+        // When the user clicks on <span> (x), close the modal
+        span.onclick = function() {
+        modal.style.display = "none";
+        }
+        // When the user clicks anywhere outside of the modal, close it
+        window.onclick = function(event) {
+            if (event.target === modal) {
+                modal.style.display = "none";
+            }
+        }
+    }
+
+    render() {
+        return (
+        <div className="body">
+            <div className="column">
+                <form className="report">
+                    <h3 style={{color: '#CD7320', fontSize: '28px', fontWeight: '200', textDecoration: 'underline'}}>Sexual Assault Report</h3>
+                    <label for="university">What college/university do you attend?</label>
+                    <input type="text" id="university"></input>
+                    <label for="email">Please provide your school email address (must be .edu)</label>
+                    <input type="text" id="email" ></input>
+                    <label for="location">Location, date, and time of the event</label>
+                    <input type="text" id="location"></input>
+                    <label for="offender">
+                        If known, please state the person responsible for the harassment. 
+                        If not, provide a description of the offender.
+                    </label>
+                    <input type="textarea" id="offender"></input>
+                    <label for="starttime">When did it start?</label>
+                    <input type="text" id="starttime"></input>
+                    <label for="endtime">When did it end?</label>
+                    <input type="text" id="endtime"></input>
+                    <label for="witnesses">
+                        Were there any witnesses? 
+                        If so, please provide name(s) or descriptions.
+                    </label>
+                    <input type="textarea" id="witnesses"></input>
+                    <label for="attachment">
+                        If you have any, please upload any documentation to help
+                        support your case (medical records, photos/videos, etc).
+                    </label>
+                    <input type="file" id="attachment"></input>
+
+                    <div className="row" style={{justifyContent: 'space-between', padding: '0px', width: '100%'}}>
+                        <a href="#" ><button className="submit-button">Exit</button></a>
+                        <button action="submit"  onClick={()=>{this.createModal()}}className="submit-button">Submit</button>
+                    </div>
+                    
+                </form>
+            </div>
+
+            <div id="myModal" className="modal">
+
+                <div className="modal-content1">
+                    <div style={{borderBottom: '3px black double'}}>
+                        <span className="close-modal">&times;</span>
+                    </div>
+                    <div className="column" style={{padding: '10px 16px'}}>
+                        <h1 style={{color: 'white'}}>Thank you for your submission!</h1>
+                    </div>
+                    <div className="column" style={{borderRadius: '20px', padding: '5% 20%', backgroundColor: '#FEF8F0'}}>
+                        <div id="modal-text" style={{textDecoration: 'underline'}}>Your report number:</div>
+                        <h3 style={{color: '#CD7320'}}>############</h3>
+                        <div id="modal-text">You will receive an email shortly regarding your report's progress.</div>
+                    </div>
+                    <div>
+                        <a href="#Track"><button className="report-button" style={{width: 'auto'}}>Track My Report</button></a>
+                    </div>
+                </div>
+            </div>
+        </div>
+        )
+    }
+}

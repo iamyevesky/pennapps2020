379e0855da13f369ff37113976a78f0c4c239310
Sena Yevenyo <yevesky2020@gmail.com>
2020-09-13 01:30:34 -0400

    Add failed backend

--- pennapps/settings.py
+++ pennapps/settings.py
@@ -136,3 +136,8 @@ WEBPACK_LOADER = {
             'STATS_FILE': os.path.join(BASE_DIR, 'webpack-stats.dev.json'),
         }
 }
+
+SITE_ROOT = os.path.dirname(os.path.realpath(__file__))
+STATICFILES_DIRS = (
+  os.path.join(SITE_ROOT, 'static'),
+)
--- webapp/admin.py
+++ webapp/admin.py
@@ -7,5 +7,5 @@ from .models import Report
 
 @admin.register(Report)
 class ReportAdmin(admin.ModelAdmin):
-    list_display = ('tracking_number', 'date_of_report', 'email', 'start_date', 'end_date')
-    fields = ['date_of_report', "tracking_number", 'email', ('start_date', 'end_date')]
+    list_display = ('tracking_number', 'email', 'start_date', 'end_date')
+    fields = ["tracking_number", 'email', ('start_date', 'end_date')]

1a1415b62d53775a84a80f7160c62fb64269016b
Sena Yevenyo <yevesky2020@gmail.com>
2020-09-13 01:38:37 -0400

    Add links to REsource page

--- src/components/Resources.js
+++ src/components/Resources.js
@@ -8,11 +8,11 @@ function Resources() {
         <h2 className="title2" style={{color: '#CD7320', textDecoration: 'none'}}>Resources</h2>
       </div>
       <div className="round-column" style={{margin: '0px 32px'}}>
-            <a className="link" style={{textDecoration: 'underline', color: 'black', margin: '16px'}}>What is sexual assault?</a>
-            <a className="link" style={{textDecoration: 'underline', color: 'black', margin: '16px'}}>What to do after you have experienced sexual assault</a>
-            <a className="link" style={{textDecoration: 'underline', color: 'black', margin: '16px'}}>How to file a police report</a>
-            <a className="link" style={{textDecoration: 'underline', color: 'black', margin: '16px'}}>Counseling and sexual assault hot lines</a>
-            <a className="link" style={{textDecoration: 'underline', color: 'black', margin: '16px'}}>Title IX</a>
+            <a href="https://www.rainn.org/articles/sexual-assault" className="link" style={{textDecoration: 'underline', color: 'black', margin: '16px'}}>What is sexual assault?</a>
+            <a href="https://www.rainn.org/after-sexual-assault" className="link" style={{textDecoration: 'underline', color: 'black', margin: '16px'}}>What to do after you have experienced sexual assault</a>
+            <a href="https://www.rainn.org/articles/reporting-law-enforcement" className="link" style={{textDecoration: 'underline', color: 'black', margin: '16px'}}>How to file a police report</a>
+            <a href="https://www.rainn.org/about-national-sexual-assault-telephone-hotline" className="link" style={{textDecoration: 'underline', color: 'black', margin: '16px'}}>Counseling and sexual assault hot lines</a>
+            <a href="https://www.rainn.org/articles/title-ix" className="link" style={{textDecoration: 'underline', color: 'black', margin: '16px'}}>Title IX</a>
       </div>
     </div>
   );

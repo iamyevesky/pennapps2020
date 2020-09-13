71cee7f1b22fc632cdc362799f455e4a2abf5a49
Kate Land <kateqland@gmail.com>
2020-09-12 23:23:58 -0400

    updated resources page

--- src/components/Resources.js
+++ src/components/Resources.js
@@ -5,9 +5,14 @@ function Resources() {
   return (
     <div className="body">
       <div className="column">
-        <p>
-            add resources here
-        </p>
+        <h2 className="title2" style={{color: '#CD7320', textDecoration: 'none'}}>Resources</h2>
+      </div>
+      <div className="round-column" style={{margin: '0px 32px'}}>
+            <a className="link" style={{textDecoration: 'underline', color: 'black', margin: '16px'}}>What is sexual assault?</a>
+            <a className="link" style={{textDecoration: 'underline', color: 'black', margin: '16px'}}>What to do after you have experienced sexual assault</a>
+            <a className="link" style={{textDecoration: 'underline', color: 'black', margin: '16px'}}>How to file a police report</a>
+            <a className="link" style={{textDecoration: 'underline', color: 'black', margin: '16px'}}>Counseling and sexual assault hot lines</a>
+            <a className="link" style={{textDecoration: 'underline', color: 'black', margin: '16px'}}>Title IX</a>
       </div>
     </div>
   );
